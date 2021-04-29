792372300d2f0273a1bd28a63c6924c4fcd4c602
Charlie Stokes <chstokes7@gmail.com>
2021-04-29 16:47:40 -0500

    working through troubleshooting

--- server/routes/dashboard.router.js
+++ server/routes/dashboard.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const dashboardRouter = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
