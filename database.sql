aa2bd40c3b30bc27c38a17795a8cabbf6d1a97b2
Charlie Stokes <chstokes7@gmail.com>
2021-04-29 13:41:26 -0500

    Updated database.sql file

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE shopping (
+	id SERIAL PRIMARY KEY,
+	name varchar(80),
+	quantity numeric(12,2),
+	unit varchar(50)
+);
+
+INSERT INTO "shopping" ("name", "quantity", "unit") 
+VALUES ('Milk', 1, 'Gallon'),('Eggs' ,1 ,'Dozen'),('Cake',2,'13x9'),('Coffee', 1, 'Bag');
