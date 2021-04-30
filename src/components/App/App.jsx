ff32a27ac3602eb8210b0cccba0bb00d5e5bef9b
Alex White <alexlloydwhite@gmail.com>
2021-04-30 09:00:42 -0500

    refactored POST route to ItemForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,60 +10,12 @@ import getShoppingList from '../ShoppingList/ShoppingList.jsx';
 
 
 function App() {
-    const [name, setName] = useState('');
-    const [quantity, setQuantity] = useState('');
-    const [unit, setUnit] = useState('');
-
-    useEffect(() => {
-        getShoppingList()
-    }, [])
-
-    const addItem = () => {
-
-        // wrapping new item into an object
-        const itemToAdd = {
-            name: name,
-            quantity: Number(quantity),
-            unit: unit,
-        } // end itemToAdd
-
-        // fact checking
-        console.log('newItem is:', itemToAdd);
-
-        //post route for adding new item
-        axios({
-            method: 'POST',
-            url: '/list',
-            data: itemToAdd
-        })
-            .then(response => {
-                console.log('response from server', response);
-            })
-            .catch(error => {
-                console.log('error sending newItem to server:', error);
-            });
-    } // end addItem
-
-    // called when submit button pressed
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addItem();
-    } // end handleSubmit
 
     return (
         <div className="App">
             <Header />
             <main>
-
-                <ItemForm
-                    name={name}
-                    setName={setName}
-                    quantity={quantity}
-                    setQuantity={setQuantity}
-                    unit={unit}
-                    setUnit={setUnit}
-                    handleSubmit={handleSubmit}
-                />
+                <ItemForm />
                 <Dashboard />
                 <ShoppingList />
 
